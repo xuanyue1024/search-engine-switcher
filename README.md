83f26be652052175f12439f8f6d32263e835ec95
竹林听雨 <xuanyue2003@foxmail.com>
2025-11-07 17:33:10 +0800

    修正解释

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## 搜索引擎切换工具
 
-在搜索引擎左侧显示一个快速切换列表，节省「另开搜索引擎」和「输入关键词」的动作和时间，提高搜索效率。在原作者基础上修改了部分内容，原链接：https://greasyfork.org/zh-CN/scripts/440235。
+能在搜索引擎左侧显示一个快速切换列表的油猴脚本，节省「另开搜索引擎」和「输入关键词」的动作和时间，提高搜索效率。在原作者基础上修改了部分内容，[原链接](https://greasyfork.org/zh-CN/scripts/440235)
+
+Greasy Fork安装地址: [https://greasyfork.org/zh-CN/scripts/547014](https://greasyfork.org/zh-CN/scripts/547014)
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         搜索引擎切换工具
-// @namespace    http://tampermonkey.net/
-// @version      0.1.31
-// @description  在搜索引擎左侧显示一个快速切换列表，节省「另开搜索引擎」和「输入关键词」的动作和时间，提高搜索效率。在原作者基础上修改了部分内容，原链接：https://greasyfork.org/zh-CN/scripts/440235。
+// @namespace    https://greasyfork.org/zh-CN/users/1143571-xuanyue1024
+// @version      0.1.32
+// @description  能在搜索引擎左侧显示一个快速切换列表的油猴脚本，节省「另开搜索引擎」和「输入关键词」的动作和时间，提高搜索效率。在原作者基础上修改了部分内容，[原链接](https://greasyfork.org/zh-CN/scripts/440235)
 // @author       竹林听雨, rockucn
 
 // @match        *://www.baidu.com/s*
